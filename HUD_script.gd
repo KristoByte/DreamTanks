615bc62a68d6028bd706b386ee6787c008b21098
kristopherG <kegarcia2003@gmail.com>
2024-09-19 13:51:06 -0500

    Added more to the HUD script as well as semi started working on the background

--- HUD_script.gd
+++ HUD_script.gd
@@ -12,7 +12,18 @@ func show_game_over():
 	show_message ("Game Over")
 	await $MessageTimer.timeout
 
-	$Message.text = "Dodge the Creeps!"
+	$Message.text = "Stay Alive!"
 	$Message.show()
 	await get_tree().create_timer(1.0).timeout
 	$StartButton.show()
+
+func update_score(score):
+	$ScoreLabel.text = str(score)
+	
+	
+func _on_start_button_pressed():
+	$StartButton.hide()
+	start_game.emit()
+
+func _on_message_timer_timeout():
+	$Message.hide()
