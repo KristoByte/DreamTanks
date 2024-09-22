e7a97cadfc71bfbfe341ea44206be75dfd764303
valgrigar <vgrigar@yahoo.com>
2024-09-22 16:39:38 -0500

    created main scene and edited player animations

--- player.gd
+++ player.gd
@@ -1,9 +1,13 @@
 extends Area2D
+signal hit
 
 @export var Bullet : PackedScene
 
-@export var speed = 10
+@export var speed = 100
 var screen_size
+var aim_direction = 0
+var flip_direction = 0
+var hit_count = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -16,9 +20,13 @@ func _process(delta):
 	var velocity = Vector2.ZERO
 	if Input.is_action_pressed("move_right"):
 		velocity.x += 1
+		flip_direction = 0
 	if Input.is_action_pressed("move_left"):
 		velocity.x -= 1
-	#add input actions for aim and shoot (add animation for aim that makes frame 1 the aim up)
+		flip_direction = 1
+		
+	if Input.is_action_just_pressed("shoot"):
+		shoot()
 	
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
@@ -29,28 +37,95 @@ func _process(delta):
 	position += velocity * delta
 	position = position.clamp(Vector2.ZERO, screen_size)
 	
-	if velocity.x != 0:
-		$AnimatedSprite2D.animation = "default"
-		if Input.is_action_pressed("aim_up"):
-			$AnimatedSprite2D.animation = "default_aim_up"
-		if Input.is_action_pressed("aim_down"):
-			$AnimatedSprite2D.animation = "default"
+	if velocity.x > 0:
+		#$AnimatedSprite2D.animation = "default"
+		flip_direction = 0
+		$AnimatedSprite2D.flip_h = false
+		if Input.is_action_just_pressed("aim_up"):
+			if hit_count == 0:
+				$AnimatedSprite2D.animation = "default_aim_up"
+			if hit_count == 1:
+				$AnimatedSprite2D.animation = "one_hit_aim_up"
+			if hit_count == 2:
+				$AnimatedSprite2D.animation = "two_hit_aim_up"
+			aim_direction = 1
+		if Input.is_action_just_pressed("aim_down"):
+			if hit_count == 0:
+				$AnimatedSprite2D.animation = "default"
+			if hit_count == 1:
+				$AnimatedSprite2D.animation = "one_hit"
+			if hit_count == 2:
+				$AnimatedSprite2D.animation = "two_hit"
+			aim_direction = 0
 		#make sure to add another if to check if player has been hit to play other animations
 	
 	if velocity.x == 0:
-		$AnimatedSprite2D.animation = "default"
-		if Input.is_action_pressed("aim_up"):
-			$AnimatedSprite2D.animation = "default_aim_up"
-		if Input.is_action_pressed("aim_down"):
-			$AnimatedSprite2D.animation = "default"
+		#$AnimatedSprite2D.animation = "default"
+		if Input.is_action_just_pressed("aim_up"):
+			aim_direction = 1
+			if hit_count == 0:
+				$AnimatedSprite2D.animation = "default_aim_up"
+			if hit_count == 1:
+				$AnimatedSprite2D.animation = "one_hit_aim_up"
+			if hit_count == 2:
+				$AnimatedSprite2D.animation = "two_hit_aim_up"
+		
+		if Input.is_action_just_pressed("aim_down"):
+			if hit_count == 0:
+				$AnimatedSprite2D.animation = "default"
+			if hit_count == 1:
+				$AnimatedSprite2D.animation = "one_hit"
+			if hit_count == 2:
+				$AnimatedSprite2D.animation = "two_hit"
+			aim_direction = 0
+		
+		if flip_direction == 1:
+			$AnimatedSprite2D.flip_h = true
+		if flip_direction == 0:
+			$AnimatedSprite2D.flip_h = false
+		
 	
 	 
-	if Input.is_action_just_pressed("shoot"):
-		shoot()
+	if velocity.x < 0:
+		$AnimatedSprite2D.flip_h = true
+		flip_direction = 1
+	
 			
 			
 func shoot():
-	var b = Bullet.instantiate()
-	add_child(b)
-	b.transform = $Muzzle.transform
+	if flip_direction == 0:
+		if aim_direction == 1:
+			var b = Bullet.instantiate()
+			owner.add_child(b)
+			b.transform = $Muzzle2.global_transform
+		if aim_direction == 0:
+			var b = Bullet.instantiate()
+			owner.add_child(b)
+			b.transform = $Muzzle.global_transform 
+	if flip_direction == 1:
+		if aim_direction == 1:
+			var b = Bullet.instantiate()
+			owner.add_child(b)
+			b.transform = $Muzzle4.global_transform
+		if aim_direction == 0:
+			var b = Bullet.instantiate()
+			owner.add_child(b)
+			b.transform = $Muzzle3.global_transform 
+	
+
+
+func _on_body_entered(body):
+	hit_count += 1
+	if hit_count == 3:
+		hide()
+		hit.emit()
+		$CollisionShape2D.set_deferred("disabled", true)
+	#need to work on obstacles next so you can test the hit animations
+	#maybe add animation for the "part" falling off?
+	#need to make fram for aim up for one and two hit (maybe get new frames for all?)
+	
+	
+	
+	
+	
 	
