33fb8a51ce5e9d07e08f7574fff895bc5a6cf063
valgrigar <vgrigar@yahoo.com>
2024-10-03 13:47:44 -0500

    fixed obstacle spawning and made explosion scene

--- main.gd
+++ main.gd
@@ -31,24 +31,9 @@ func _on_obstacle_timer_timeout():
 	
 		var obstacle_spawn_location = $ObstaclePath/ObstacleSpawnLocation
 		obstacle_spawn_location.progress_ratio = randf()
-
-	# Set the mob's direction perpendicular to the path direction.
-		var direction = obstacle_spawn_location.rotation + PI / 2
-
-	# Set the mob's position to a random location.
 		obstacle.position = obstacle_spawn_location.position
-
-	# Add some randomness to the direction.
-		direction += randf_range(-PI / 4, PI / 4)
-		obstacle.rotation = direction
-
-	# Choose the velocity for the mob.
-		var velocity = Vector2(randf_range(150.0, 250.0), 0.0)
-		obstacle.linear_velocity = velocity.rotated(direction)
-
 	# Spawn the mob by adding it to the Main scene.
 		add_child(obstacle)
-	
 		objCount += 1
 		
 
